e2f8a8fba3df86f61a0e375879b275e184b64d03
Avika Agarwal <agkaviz.097@gmail.com>
2026-09-10 21:35:44 +0530

    commit my code

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/routes.js';
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
