9fafd34a32130a464b525ac64b7ba8c29ab43c17
Avika Agarwal <agkaviz.097@gmail.com>
2026-09-11 10:29:00 +0530

    Add gitignore

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
-import mysql from "mysql2";
-import dotenv from "dotenv";
+import mysql from "mysql2/promise";
 
-dotenv.config();
+export const connectDB = async () => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
 
-const db = await mysql.createConnection({
-    host: process.env.DB_localhost,
-    user: process.env.DB_root,
-    password: process.env.DB_12345678,
-    database: process.env.DB_event_ticketing,
-    port: process.env.DB_3306
-  });
+        console.log("MySQL database connected");
 
-console.log("MySQL connected successfully");
-
-export default db;
+        return connection;
+    } catch (error) {
+        console.log("Database connection failed:", error.message);
+    }
+};
--- config/models/event.module.js
+++ config/models/event.module.js
@@ -1,2 +1,2 @@
-import mysql from "mysql2";
+
 
